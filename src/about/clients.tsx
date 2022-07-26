a8b624db1a9e9b3e38898476d5c53fbb513f0813
Aleksandr Brenchev <abrenchev@griddynamics.com>
2022-07-26 15:59:12 +0200

    Removed excessive logo downscaling for clients section

--- src/about/clients.tsx
+++ src/about/clients.tsx
@@ -8,12 +8,22 @@ export const Clients = () => {
           Some of our clients
         </h2>
 
-        <div className="flex flex-wrap gap-10 items-center">
-          <img className="w-full md:w-1/5-gap-10" src="/src/assets/the-verge-logo.svg" alt="The Verge logo" />
-          <img className="w-full md:w-1/5-gap-10" src="/src/assets/jakarta-post-logo.svg" alt="Jakarta Post logo" />
-          <img className="w-full md:w-1/5-gap-10" src="/src/assets/the-guardian-logo.svg" alt="The Guardian logo" />
-          <img className="w-full md:w-1/5-gap-10" src="/src/assets/techradar-logo.svg" alt="Techradar logo" />
-          <img className="w-full md:w-1/5-gap-10" src="/src/assets/gadgets-now-logo.svg" alt="Gadgets Now logo" />
+        <div className="flex gap-10 flex-wrap flex-row items-center">
+          <div className="w-full md:w-1/5-gap-10 min-w-max grow-1">
+            <img className="mx-auto" width="165" height="28" src="/src/assets/the-verge-logo.svg" alt="The Verge logo" />
+          </div>
+          <div className="w-full md:w-1/5-gap-10 min-w-max grow-1">
+            <img className="mx-auto" width="184" height="23" src="/src/assets/jakarta-post-logo.svg" alt="Jakarta Post logo" />
+          </div>
+          <div className="w-full md:w-1/5-gap-10 min-w-max grow-1">
+            <img className="mx-auto" width="180" height="28" src="/src/assets/the-guardian-logo.svg" alt="The Guardian logo" />
+          </div>
+          <div className="w-full md:w-1/5-gap-10 min-w-max grow-1">
+            <img className="mx-auto" width="165" height="28" src="/src/assets/techradar-logo.svg" alt="Techradar logo" />
+          </div>
+          <div className="w-full md:w-1/5-gap-10 min-w-max grow-1">
+            <img className="mx-auto" width="98" height="45" src="/src/assets/gadgets-now-logo.svg" alt="Gadgets Now logo" />
+          </div>
         </div>
       </Frame>
     </section>
