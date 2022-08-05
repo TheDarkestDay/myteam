b948d6e8966dfce9c13c662a2ce773e5b27ca9f4
Aleksandr Brenchev <abrenchev@griddynamics.com>
2022-08-05 11:13:53 +0200

    Implemented graphics for Contacts page

--- src/contact/contact-page.tsx
+++ src/contact/contact-page.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 import { Button, FeatureSection, Frame, InputField } from '../common';
 
 type Props = {
@@ -5,11 +7,31 @@ type Props = {
 }
 
 export const ContactPage = (props: Props) => {
+  const topGraphicsClassName = `
+    before:hidden md:before:block
+    before:absolute before:top-0 before:left-0
+    before:bg-graphics-contact-top before:bg-no-repeat before:bg-right
+    before:w-50 before:h-50
+    before:-translate-x-1/2
+  `;
+
+  const bottomGraphicsClassName = `
+    after:absolute after:bottom-0 after:right-0
+    after:bg-graphics-contact-bottom after:bg-no-repeat
+    after:w-25 after:h-25 lg:after:h-50
+  `;
+
+  const sectionClassName = classNames(
+    'relative bg-primaryDark',
+    topGraphicsClassName,
+    bottomGraphicsClassName
+  );
+
   return (
     <main>
-      <section className="bg-primaryDark">
-        <Frame className="flex flex-wrap gap-7 justify-center lg:justify-between">
-          <div>
+      <section className={sectionClassName}>
+        <Frame className="flex flex-wrap gap-7 justify-center">
+          <div className="w-full lg:w-1/2-gap-8">
             <h2 className="text-white text-center lg:text-left font-bold text-6xl mb-4">
               Contact
             </h2>
@@ -23,7 +45,7 @@ export const ContactPage = (props: Props) => {
             <FeatureSection titleColor="white" title="How we help drive innovation" icon="analytics" />
           </div>
 
-          <form className="w-full lg:w-2/5 lg:shrink">
+          <form className="w-full lg:w-1/2-gap-8">
             <div className="mb-6">
               <InputField className="mb-6" name="username" label="Name" />
               <InputField className="mb-6" name="email" label="Email" type="email" />
