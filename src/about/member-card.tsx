82e104b96335ef82af2b1757c6212b66bcdea0ff
Aleksandr Brenchev <abrenchev@griddynamics.com>
2022-08-07 12:36:58 +0200

    Implemented expanded quote for member card

--- src/about/member-card.tsx
+++ src/about/member-card.tsx
@@ -1,26 +1,81 @@
-import classNames from 'classnames';
+import classNames from "classnames";
+import { useState } from "preact/hooks";
+
+import { Link } from "../common";
 
 type Props = {
   name: string;
   role: string;
-  quote: string; // TODO: Implemented quote card
+  quote: string;
   photo: string;
   className?: string;
 };
 
-export const MemberCard = ({name, role, quote, className, photo}: Props) => {
-  const rootClassName = classNames('relative bg-secondaryDarkDecoration px-6 py-8 text-center', className);
+export const MemberCard = ({ name, role, quote, className, photo }: Props) => {
+  const [shouldShowQuote, setShouldShowQuote] = useState(false);
+
+  const rootClassName = classNames(
+    "relative bg-secondaryDarkDecoration px-6 py-8 text-center",
+    className
+  );
+
+  const quoteCardClassName = classNames(
+    `
+      absolute z-20 top-0 left-0
+      w-full h-full px-6 py-8
+      bg-secondaryDarkDecoration
+      transition
+    `,
+    shouldShowQuote ? "translate-y-0" : "translate-y-full"
+  );
+
+  const handleExpandButtonClick = () => {
+    setShouldShowQuote(!shouldShowQuote);
+  };
 
   return (
     <article className={rootClassName}>
-      <img src={photo} alt={`Photo of ${name}`} className="mx-auto mb-4" />
-      <p className="text-secondaryBlue text-lg">
-        {name}
-      </p>
-      <p className="text-white text-sm italic mb-7">
-        {role}
-      </p>
-      <button className="transition hover:bg-secondaryBlue rounded-full absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 font-bold text-2xl bg-primaryLight w-14 h-14" aria-label={`Show quote from ${name}`}>+</button>
+      <div className="relative overflow-y-hidden">
+        <img src={photo} alt={`Photo of ${name}`} className="mx-auto mb-4" />
+        <p className="text-secondaryBlue font-bold text-lg">{name}</p>
+        <p className="text-white text-sm italic mb-7">{role}</p>
+
+        <section className={quoteCardClassName}>
+
+          <figure className="mb-6">
+            <figcaption className="text-secondaryBlue text-lg font-bold mb-2">
+              {name}
+            </figcaption>
+
+            <blockquote className="text-white text-sm">
+              {quote}
+            </blockquote>
+          </figure>
+
+          <nav className="flex justify-center gap-4">
+            <Link href="#">
+              <img
+                src="/src/assets/twitter-logo.svg"
+                alt={`Twitter of ${name}`}
+              />
+            </Link>
+            <Link href="#">
+              <img
+                src="/src/assets/linkedin-logo.svg"
+                alt={`LinkedIn profile of ${name}`}
+              />
+            </Link>
+          </nav>
+        </section>
+      </div>
+
+      <button
+        onClick={handleExpandButtonClick}
+        className="transition hover:bg-secondaryBlue rounded-full absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 font-bold text-2xl bg-primaryLight w-14 h-14"
+        aria-label={`Show quote from ${name}`}
+      >
+        +
+      </button>
     </article>
   );
 };
--- src/about/team.tsx
+++ src/about/team.tsx
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import { Frame } from '../common';
 import { MemberCard } from './member-card';
 
+const dummyQuote = '“Empowered teams create truly amazing products. Set the north star and let them follow it.”';
+
 export const Team = () => {
   const topGraphicsClassName = `
     before:hidden before:md:block
@@ -31,12 +33,12 @@ export const Team = () => {
         </h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-12">
-          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Nikita Marks" role="Founder and CEO" photo="/src/assets/nmarks-photo.png" quote="" />
-          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Christian Duncan" role="Co-founder and COO" photo="/src/assets/cduncan-photo.png" quote="" />
-          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Cruz Hamer" role="Co-founder and CTO" photo="/src/assets/chamer-photo.png" quote="" />
-          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Drake Heaton" role="Business development lead" photo="/src/assets/dheaton-photo.png" quote="" />
-          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Griffin Wise" role="Lead Marketing" photo="/src/assets/gwise-photo.png" quote="" />
-          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Aden Allan" role="Head of talent" photo="/src/assets/aallan-photo.png" quote="" />
+          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Nikita Marks" role="Founder and CEO" photo="/src/assets/nmarks-photo.png" quote={dummyQuote} />
+          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Christian Duncan" role="Co-founder and COO" photo="/src/assets/cduncan-photo.png" quote={dummyQuote} />
+          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Cruz Hamer" role="Co-founder and CTO" photo="/src/assets/chamer-photo.png" quote={dummyQuote} />
+          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Drake Heaton" role="Business development lead" photo="/src/assets/dheaton-photo.png" quote={dummyQuote} />
+          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Griffin Wise" role="Lead Marketing" photo="/src/assets/gwise-photo.png" quote={dummyQuote} />
+          <MemberCard className="grow w-full md:w-1/2-gap-8 lg:w-1/3-gap-8" name="Aden Allan" role="Head of talent" photo="/src/assets/aallan-photo.png" quote={dummyQuote} />
         </div>
       </Frame>
     </section>
