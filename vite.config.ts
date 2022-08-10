dca7b79f08c1c10b8cba8ccdc74dd2dd29cb090c
Aleksandr Brenchev <abrenchev@griddynamics.com>
2022-08-10 18:35:28 +0200

    Trying to fix tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/myteam/',
-  resolve: {
-    alias: {
-      "react": "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat",
-      "react/jsx-runtime": "preact/jsx-runtime"
-    }
-  },
+  // resolve: {
+  //   alias: {
+  //     "react": "preact/compat",
+  //     "react-dom/test-utils": "preact/test-utils",
+  //     "react-dom": "preact/compat",
+  //     "react/jsx-runtime": "preact/jsx-runtime"
+  //   }
+  // },
   plugins: [preact()]
 })
