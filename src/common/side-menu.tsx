9ff96d32b723a3ab215fe456c152faa9ec1150dc
Aleksandr Brenchev <abrenchev@griddynamics.com>
2022-08-17 21:36:11 +0200

    Reworked mobile menu in order to get rid of invisible ghosts for VO and keyboard focus

--- src/common/side-menu.tsx
+++ src/common/side-menu.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'preact/hooks';
 import classNames from 'classnames';
 
 import { Link } from './link';
@@ -10,22 +11,49 @@ type Props = {
 };
 
 export const SideMenu = ({open, onClose}: Props) => {
+  const [isSlideIn, setIsSlideIn] = useState(false);
+  const [shouldMount, setShouldMount] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      setShouldMount(true);
+    } else {
+      setIsSlideIn(false);
+    }
+  }, [setShouldMount, setIsSlideIn, open]);
+
   const bottomGraphicsClassName = 'after:absolute after:w-50 after:h-50 after:bottom-0 after:right-0 after:bg-graphics-side-menu after:bg-no-repeat after:translate-x-1/2';
 
   const sideMenuClassName = classNames(
     'fixed h-screen right-0 top-0 bg-secondaryDarkBlue transition z-20 p-12',
-    open ? 'translate-x-0' : 'translate-x-full',
+    isSlideIn ? 'translate-x-0' : 'translate-x-full',
     bottomGraphicsClassName
   );
 
   const backdropClassName = 'fixed top-0 left-0 h-screen w-screen z-10 bg-secondaryDarkDecoration opacity-50';
 
+  const onMenuRendered = (ref: HTMLElement | null) => {
+    if (ref != null && open) {
+      setIsSlideIn(true);
+
+      ref.scrollTo(0, 0);
+    }  
+  };
+
+  const handleTransitionEnd = () => {
+    if (!open) {
+      setShouldMount(false);
+    }
+  };
+
+  if (!shouldMount) {
+    return null;
+  }
+
   return (
     <>
-      {
-        open && <div aria-hidden="true" className={backdropClassName} onClick={onClose}></div>
-      }
-      <nav className={sideMenuClassName}>
+      <div aria-hidden="true" className={backdropClassName} onClick={onClose}></div>
+      <nav ref={onMenuRendered} onTransitionEnd={handleTransitionEnd} className={sideMenuClassName}>
         <div className="text-right">
           <IconButton ariaLabel="Close menu" className="ml-auto mb-10" iconName='close' onClick={onClose} />
         </div>
